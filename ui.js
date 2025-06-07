2956706a6d14d66ffb2abf74e10bd4f073992342
Loamy88 <euan.m.obrien@gmail.com>
2025-06-06 20:14:48 -0700

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,17 @@
+// UI overlay and speedometer
+function createOverlay() {
+  const overlay = document.createElement('div');
+  overlay.id = 'overlay';
+  overlay.innerHTML = `
+    <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0,0 Q50,80 100,0" fill="black" opacity="0.4" transform="translate(0,0) scale(${window.innerWidth / 100}, 1)"/>
+    </svg>`;
+  document.body.appendChild(overlay);
+}
+
+function createSpeedometer() {
+  const speedometer = document.createElement('div');
+  speedometer.id = 'speedometer';
+  document.body.appendChild(speedometer);
+  return speedometer;
+}
