ea8d21ec4e9d583dae938c165e0fce4345602cd6
Loamy88 <euan.m.obrien@gmail.com>
2025-06-06 23:37:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -39,9 +39,9 @@ scene.add(basePlane);
 
 // Generate city grid
 const city = generateCityGrid({
-  gridSize: 8,
+  gridSize: 6,
   cellSize: 25,
-  roadWidth: 10,
+  roadWidth: 12,
   fieldChance: 0.12,
   missingRoadChance: 0.18,
   startBlock: new THREE.Vector2(0, 0)
